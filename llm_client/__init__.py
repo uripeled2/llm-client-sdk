a285b15042cdd53f2cec9e1b10104a3001689325
Uri Peled <uripeled2@gmail.com>
2023-04-25 18:17:18 +0300

    add pyproject.toml

---
+++ llm_client/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.0.1"
+
+from llm_client.llm_client_interface import LLMClientInterface
+from llm_client.llm_client.openai_client import OpenAIClient
+from llm_client.llm_client.ai21 import AI21Client
