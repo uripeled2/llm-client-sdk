b2138d804d740214ad6892879e14b0920dbf5f5a
uripeled2 <uripeled2@gmail.com>
2023-04-26 11:04:06 +0300

    fix missing imports with patrial dependencies

--- llm_client/__init__.py
+++ llm_client/__init__.py
@@ -1,5 +1,12 @@
 __version__ = "0.0.1"
 
 from llm_client.llm_client_interface import LLMClientInterface
-from llm_client.llm_client.openai_client import OpenAIClient
-from llm_client.llm_client.ai21 import AI21Client
+
+try:
+    from llm_client.llm_client.openai_client import OpenAIClient
+except ImportError:
+    pass
+try:
+    from llm_client.llm_client.ai21 import AI21Client
+except ImportError:
+    pass
