dc5b5857e4922b2f2c18f2eea4593c0eccadd342
uripeled2 <uripeled2@gmail.com>
2023-05-12 22:33:28 +0300

    update examples

--- examples/main.py
+++ examples/main.py
@@ -1,30 +1,52 @@
 import os
 
 from aiohttp import ClientSession
+from transformers import AutoModelForCausalLM, AutoModelForSeq2SeqLM, AutoTokenizer
 
-from llm_client import BaseLLMClient, OpenAIClient, AI21Client
-from llm_client.llm_api_client.openai_client import ChatMessage, Role
+from llm_client import LLMAPIClientConfig, LLMAPIClientType, get_llm_api_client, ChatMessage, Role, LocalClient, \
+    LocalClientConfig
 
 
-def get_llm_api_client() -> BaseLLMClient:
-    if os.getenv("API_KEY") is None:
-        raise ValueError("API_KEY environment variable is not set when using api client")
-    if os.getenv("LLM_CLIENT") == "openai":
-        print("Using OpenAI client")
-        return OpenAIClient(os.environ["API_KEY"], ClientSession(), default_model="davinci")
-    elif os.getenv("LLM_CLIENT") == "ai21":
-        print("Using AI21 client")
-        return AI21Client(os.environ["API_KEY"], ClientSession(), default_model="j2-grande")
-    else:
-        raise ValueError("Unknown LLM client type")
+LLM_CLIENT_TYPE = os.environ["LLM_CLIENT_TYPE"]
+# api client env var
+LLM_API_KEY = os.getenv("LLM_API_KEY")
+# local client env var
+MODEL_NAME_OR_PATH = os.getenv("MODEL_NAME_OR_PATH")
+TENSORS_TYPE = os.getenv("TENSORS_TYPE")
+DEVICE = os.getenv("DEVICE")
+
+
+def get_local_client_config() -> LocalClientConfig:
+    try:
+        model = AutoModelForCausalLM.from_pretrained(MODEL_NAME_OR_PATH)
+    except ValueError:
+        model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME_OR_PATH)
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME_OR_PATH)
+    return LocalClientConfig(model, tokenizer, TENSORS_TYPE, DEVICE)
+
+
+def get_open_ai_config(session: ClientSession) -> LLMAPIClientConfig:
+    return LLMAPIClientConfig(LLM_API_KEY, session, default_model="davinci")
+
+
+def get_ai21_config(session: ClientSession) -> LLMAPIClientConfig:
+    return LLMAPIClientConfig(LLM_API_KEY, session, default_model="j2-grande")
 
 
 async def main():
-    llm_client = get_llm_api_client()
+    if LLM_CLIENT_TYPE == "LOCAL":
+        llm_client = LocalClient(get_local_client_config())
+    elif LLM_CLIENT_TYPE == "OPEN_AI":
+        llm_client = get_llm_api_client(LLMAPIClientType.OPEN_AI, get_open_ai_config(ClientSession()))
+    elif LLM_CLIENT_TYPE == "AI21":
+        llm_client = get_llm_api_client(LLMAPIClientType.AI21, get_ai21_config(ClientSession()))
+    else:
+        raise ValueError("Unknown LLM client type")
+
     text = "This is indeed a test"
 
     print("number of tokens:", await llm_client.get_tokens_count(text))
-    if isinstance(llm_client, OpenAIClient):
+    if hasattr(llm_client, "chat_completion"):
         print("generated chat:", await llm_client.chat_completion(
             messages=[ChatMessage(role=Role.USER, content="Hello!")], model="gpt-3.5-turbo"))
     print("generated text:", await llm_client.text_completion(text))
